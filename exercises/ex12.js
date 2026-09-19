2d88e2fca8b96a6e49443dc84405adb5f49f88fb
steven leon <lsteven644@hamil.com>
2026-09-18 22:13:46 -0700

    exercise 12 completed

--- exercises/ex12.js
+++ exercises/ex12.js
@@ -1,7 +1,10 @@
 /*
-The for loop below has an error. Fix it!
+    The for loop below has an error. Fix it!
 
-for (let k = 0 k < 200 k++) {
+    for (let k = 0 k < 200 k++) {
+      console.log(k);
+    }
+*/
+for (let k = 0; k < 200; k++) {
   console.log(k);
 }
-*/
